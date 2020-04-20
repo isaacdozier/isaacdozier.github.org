00e3b09a1240193e15ac7021f6928664b5388f8b
Isaac Dozier <isaactdozier@gmail.com>
2020-04-20 11:27:16 -0700

    update

--- assets/custom-js/22r.js
+++ assets/custom-js/22r.js
@@ -2,7 +2,7 @@ var content = document.getElementById("content");
 
 function loadContent(f, file_type) {
 	var client = new XMLHttpRequest();
-	client.open('GET', f + '.png');
+	client.open('GET', "http://isaacdozier.com/assets/custom-img/" + f + '.png');
 	client.onreadystatechange = function() {
 	  content.innerHTML = client.responseText;
 	}
