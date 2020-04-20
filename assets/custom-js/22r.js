6389bfa9679d04392c73a2f8e8f5e3c6bd3d6c9e
Isaac Dozier <isaactdozier@gmail.com>
2020-04-20 12:33:39 -0700

    Update 22r.js

--- assets/custom-js/22r.js
+++ assets/custom-js/22r.js
@@ -1,4 +1,5 @@
 function changeImg(f) {
 	var src = "http://isaacdozier.com/assets/custom-img/" + f + '.png';
 	 document.getElementById("content").src = src;
+	 document.getElementById("content").scrollIntoView()
 }
